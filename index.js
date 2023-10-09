3f4ecf191de9e48f18794b0013807cc1dd2bf573
Ivan Robles <irae45@gmail.com>
2023-10-08 20:31:23 -0600

    refactor of play 3 sounds at the same time.

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const sound_1  = new Audio();
-const source_1  = document.createElement("source");
-source_1.type = "audio/ogg";
-source_1.src  = "audio/cow.ogg";
-sound_1.appendChild(source_1);
-
-var sound_2  = new Audio();
-var source_2  = document.createElement("source");
-source_2.type = "audio/ogg";
-source_2.src  = "audio/dog.ogg";
-sound_2.appendChild(source_2);
-
-var sound_3  = new Audio();
-var source_3  = document.createElement("source");
-source_3.type = "audio/ogg";
-source_3.src  = "audio/rooster.ogg";
-sound_3.appendChild(source_3);
-
 const playButton = document.getElementById('play-button');
-playButton.addEventListener('click', () => {
-  sound_1.play(); sound_2.play(); sound_3.play();
-});
+
+function createAudioSources(...sources) {
+  sources.forEach((source) => {
+    const sound = new Audio();
+    const sourceElement = document.createElement('source');
+    sourceElement.type = 'audio/ogg';
+    sourceElement.src = source;
+    sound.appendChild(sourceElement);
+
+    playButton.addEventListener('click', () => {
+      sound.play();
+    });
+  });
+}
+
+createAudioSources('audio/cow.ogg', 'audio/dog.ogg', 'audio/rooster.ogg');
